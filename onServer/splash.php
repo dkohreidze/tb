95412e9f02b8026c1c1b765da49472bae19a25cb
jstockhoff3 <jstockhoff3@gatech.edu>
2014-10-30 16:17:14 -0400

    White Logo and Header Added Back

--- onServer/splash.php
+++ onServer/splash.php
@@ -97,10 +97,10 @@
 
 ?>
 
-    <!-- <nav class="navbar navbar-default navbar-static-top" role="navigation">
-      <div class="container-fluid"> -->
+    <nav class="navbar navbar-default navbar-static-top" role="navigation">
+      <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
-       <!--  <div class="navbar-header">
+        <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
@@ -108,13 +108,13 @@
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="#">Tolerance Break</a>
-        </div> -->
+        </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
-       <!--  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav"></ul> -->
-       <!-- </div> --><!-- /.navbar-collapse -->
-      <!-- </div> --><!-- /.container-fluid -->
-    <!--</nav> -->     
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav"></ul>
+       </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>     
 
     <div class="container-fluid" id="main-video">
       <video width="100%" height="" autoplay>
