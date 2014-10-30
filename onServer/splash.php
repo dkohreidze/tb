1b41b4e52d8cec0d8c06db57fe616e24e21b20a4
jstockhoff3 <jstockhoff3@gatech.edu>
2014-10-30 17:03:36 -0400

    Added Header Accent Files (not on page though)

--- onServer/splash.php
+++ onServer/splash.php
@@ -108,6 +108,8 @@
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="#">Tolerance Break</a>
+          <div class="navbar-accent" href="#"></div>
+
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
